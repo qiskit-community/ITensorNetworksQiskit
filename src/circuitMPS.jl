3a03eda347a8495964243a5d20d14fde534f0062
Ben Jaderberg <14994219+bjader@users.noreply.github.com>
2024-07-16 15:16:06 +0100

    Overlap between MPSs only works when generated from same grid of site indices.

--- src/circuitMPS.jl
+++ src/circuitMPS.jl
@@ -62,13 +62,10 @@ function circuitMPS(L, gates)
 
 end
 
-function mps_from_circuit(L, gates, chi)
+function mps_from_circuit(L, gates, chi, s)
     if startswith(gates, "[")
         gates = eval(Meta.parse(gates))
     end
-    #Build the graph that reflects our tensor network
-    g = named_grid((L, 1))
-    s = siteinds("S=1/2", g)
     #Initialise the tensor network, all qubits down (in Z basis)
     ψ = ITensorNetwork(v -> "↑", s)
     #Maximum bond dimension and the SVD cutoff to use
@@ -89,45 +86,28 @@ function mps_from_circuit(L, gates, chi)
     return ψ, bpc
 end
 
+function mps_from_circuit(L, gates, chi)
+    s = generate_siteindices(L)
+    return mps_from_circuit(L, gates, chi, s)
+end
+
 function mps_from_circuit(L, gates)
     return mps_from_circuit(L, gates, 10)
 end
 
-function overlap_with_zero_from_circ(L, gates)
-    # Can't seem to get this to work when inputting a ψ instead of entire circuit
-    # juliacall.JuliaError: AssertionError: issetequal(flatten_siteinds(bra), flatten_siteinds(ket))
-    if startswith(gates, "[")
-        gates = eval(Meta.parse(gates))
-    end
-
+function generate_siteindices(L)
+    #Build the graph that reflects our tensor network
     g = named_grid((L, 1))
     s = siteinds("S=1/2", g)
-    ψ = ITensorNetwork(v -> "↑", s)
-    ψref = ITensorNetwork(v -> "↑", s)
-    maxdim, cutoff = 10, 1e-14
-    apply_kwargs = (; maxdim, cutoff)
-    bp_update_kwargs = (;)
-    bpc = build_bp_cache(ψ; bp_update_kwargs...)
-
-    for gate in gates
-        o = gate_to_itensor(gate, s)
-        ψ, bpc = apply(o, ψ, bpc; apply_kwargs...)
-        bpc = update(bpc; bp_update_kwargs...)
-    end
-
-    f = sq_overlap(ψ, ψref)
-    return f
+    return s
 end
 
-function overlap_with_zero(L, ψ)
-    # Can't seem to get this to work when inputting a ψ instead of entire circuit
-    # juliacall.JuliaError: AssertionError: issetequal(flatten_siteinds(bra), flatten_siteinds(ket))
-    g = named_grid((L, 1))
-    s = siteinds("S=1/2", g)
+function overlap_with_zero(ψ, s)
     ψref = ITensorNetwork(v -> "↑", s)
     f = sq_overlap(ψ, ψref)
     return f
 end
+
 
 function sigmaz_expectation(ψ, sites)
     sites_tuples = [(n, 1) for n in sites]
@@ -137,36 +117,4 @@ end
 function two_site_rdm(ψ, bpc, site1, site2)
      ρ = two_site_rdm(ψ, (site1, 1), (site2, 1), (cache!) = Ref(bpc))
      return ρ
-end
-
-function overlap_with_increasing_chi(L, gates, chis)
-    # This function fails with the same error as overlap_with_zero. Understanding how to fix that
-    # error would get this to work. But if we fix overlap_with_zero we don't need this function
-    # as it's cleaner to build all the ψs in Python and call directly to sq_overlap
-    if startswith(gates, "[")
-        gates = eval(Meta.parse(gates))
-    end
-    ψ_list = ITensorNetwork[]
-    overlaps = zeros(0)
-    for chi in chis
-        g = named_grid((L, 1))
-        s = siteinds("S=1/2", g)
-        ψ = ITensorNetwork(v -> "↑", s)
-        maxdim, cutoff = chi, 1e-14
-        apply_kwargs = (; maxdim, cutoff)
-        bp_update_kwargs = (;)
-        bpc = build_bp_cache(ψ; bp_update_kwargs...)
-
-        for gate in gates
-            o = gate_to_itensor(gate, s)
-            ψ, bpc = apply(o, ψ, bpc; apply_kwargs...)
-            bpc = update(bpc; bp_update_kwargs...)
-        end
-        push!(ψ_list, ψ)
-    end
-    println(ψ_list)
-    for ψ in ψ_list
-        append!(overlaps, sq_overlap(ψ, ψ_list[end]))
-    end
-    return overlaps
 end
