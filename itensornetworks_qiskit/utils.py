507592401ed170c77797a6d6b7d4c2722cb119be
Kate Marshall <kvcmarshall@gmail.com>
2024-07-24 09:49:47 +0100

    updated qiskit_circ_to_it_circ

--- itensornetworks_qiskit/utils.py
+++ itensornetworks_qiskit/utils.py
@@ -71,40 +71,32 @@ def qiskit_circ_to_itn_circ_2d(qc: QuantumCircuit):
 # Again, this is very similar to the functions as above but the gates need to be a different form.
 # Instead of the index listed as [({qubit}, 1)] or [({qubit}),] it needs to be simply {qubit}
 def qiskit_circ_to_it_circ(qc: QuantumCircuit):
-    gates = '['
-    for qiskit_gate in qc:
-        if qiskit_gate.operation.name == 'u':
-            qubit = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[0])
-            theta, phi, lam = qiskit_gate.operation.params
-            gates += f'("Rn", {qubit}, (θ = {theta}, ϕ = {phi}, λ = {lam})), '
-        elif qiskit_gate.operation.name == 'rx':
-            qubit = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[0])
-            theta, = qiskit_gate.operation.params
-            gates += f'("Rx", {qubit}, (θ = {theta},)), '
-        elif qiskit_gate.operation.name == 'ry':
-            qubit = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[0])
-            theta, = qiskit_gate.operation.params
-            gates += f'("Ry", {qubit}, (θ = {theta},)), '
-        elif qiskit_gate.operation.name == 'rz':
-            qubit = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[0])
-            theta, = qiskit_gate.operation.params
-            gates += f'("Rz", {qubit}, (θ = {theta},)), '
-        elif qiskit_gate.operation.name == 't':
-            qubit = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[0])
-            gates += f'("T", {qubit}), '
-        elif qiskit_gate.operation.name == 'cx':
-            ctrl = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[0])
-            tgt = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[1])
-            gates += f'("CX", {ctrl},{tgt}), '
-        elif qiskit_gate.operation.name == 'swap':
-            ctrl = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[0])
-            tgt = jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[1])
-            gates += f'("SWAP", {ctrl},{tgt}), '
-        else:
-            raise ValueError('Unknown gate')
+    gate_formats = {
+        "u": lambda qubits, params: f'("Rn", {qubits[0]}, (θ = {params[0]}, ϕ = {params[1]}, λ = {params[2]}))',
+        "rx": lambda qubits, params: f'("Rx", {qubits[0]}, (θ = {params[0]},))',
+        "ry": lambda qubits, params: f'("Ry", {qubits[0]}, (θ = {params[0]},))',
+        "rz": lambda qubits, params: f'("Rz", {qubits[0]}, (θ = {params[0]},))',
+        "t": lambda qubits, _: f'("T", {qubits[0]})',
+        "cx": lambda qubits, _: f'("CX", {qubits[0]}, {qubits[1]})',
+        "swap": lambda qubits, _: f'("SWAP", {qubits[0]}, {qubits[1]})',
+    }
 
-    gates += ']'
-    return gates
+    gates = []
+
+    for qiskit_gate in qc:
+        name = qiskit_gate.operation.name
+        qubits = [
+            jl_qubit_int_from_qiskit_qubit_obj(qiskit_gate.qubits[i])
+            for i in range(len(qiskit_gate.qubits))
+        ]
+        params = qiskit_gate.operation.params
+
+        if name in gate_formats:
+            gates.append(gate_formats[name](qubits, params))
+        else:
+            raise ValueError(f"Unknown gate: {name}")
+
+    return "[" + ", ".join(gates) + "]"
 
 def extract_itn_graph(g):
     output_capture = io.StringIO()
