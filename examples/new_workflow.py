627be7d9cc150f02f88757f2a52cd315908a587f
MarcDrudis <MarcSanzDrudis@outlook.com>
2025-11-13 13:49:18 +0100

    First commit

---
+++ examples/new_workflow.py
@@ -0,0 +1,28 @@
+from juliacall import Main as jl
+from qiskit.quantum_info import SparsePauliOp
+from itensornetworks_qiskit.marc import parse_circuit, graph_from_edges, graph_to_grid
+from qiskit.circuit.library import real_amplitudes
+import numpy as np
+
+
+jl.seval("using ITensorNetworksQiskit")
+
+
+#### Main
+
+qc = real_amplitudes(4, entanglement="circular")
+# qc.pauli("XXXY", [0, 1, 2, 3])
+qc.assign_parameters(np.arange(qc.num_parameters), inplace=True)
+
+circuit, qiskit_connectivity = parse_circuit(qc)
+graph = graph_from_edges(qiskit_connectivity)
+qubit_map = graph_to_grid(graph, 10)
+
+
+bpc, error = jl.tn_from_qiskit_circuit(circuit, qubit_map, qiskit_connectivity)
+print("Sampling...")
+samples = jl.sample_psi(bpc, 50, 5, 5)
+samples_qiskit = []
+for sample in samples:
+    samples_qiskit.append(([jl.get(sample, coord, None) for _, coord in qubit_map]))
+print(samples_qiskit)
--- src/ITensorNetworksQiskit.jl
+++ src/ITensorNetworksQiskit.jl
@@ -1,20 +1,27 @@
 module ITensorNetworksQiskit
 
-include("imports.jl")
-include("utils.jl")
-export build_graph_from_gates
-export overlap_with_zero
-
-# heavy hex circuit example 
-# heavy hex graph generated at runtime
-include("circuitHeavyHex.jl")
-export generate_graph
-export tn_from_circuit
-export pauli_expectation
-export pauli_expectation_boundarymps
-export get_first_edge_rdm_2d
-
+# include("imports.jl")
+# include("utils.jl")
+# export build_graph_from_gates
+# export overlap_with_zero
+#
+# # heavy hex circuit example 
+# # heavy hex graph generated at runtime
+# include("circuitHeavyHex.jl")
+# export generate_graph
+# export tn_from_circuit
+# export pauli_expectation
+# export pauli_expectation_boundarymps
+# export get_first_edge_rdm_2d
+#
 include("sample.jl")
 export sample_psi
+export sample_psi_new
+
+include("marc.jl")
+export get_graph
+export translate_circuit
+export py_translate
+export tn_from_qiskit_circuit
 
 end  # module ITensorNetworksQiskit
